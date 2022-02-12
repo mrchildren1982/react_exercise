2f2f7528077782c302ee25f0dff6ceb5f55f7bab
MM <tbk40504@outlook.jp>
2022-02-12 19:35:40 +0900

    various css style

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import { ColoredMessage } from "./components/ColoredMessage";
 import { useState } from "react";
-import { CssModules } from "./components/CssModules";
+// import { CssModules } from "./components/CssModules";
+// import { StyledComponents } from "./components/StyledComponents";
+import { Emotion } from "./components/Emotion";
 // import { useEffect } from "react";
 export const App = () => {
-  const [num, setNum] = useState(0);
+  // const [num, setNum] = useState(0);
 
-  const onClickButton = () => {
-    // setNum(num + 1);
-    console.log("レンダリング");
-    setNum((prev) => prev + 1);
-  };
+  // const onClickButton = () => {
+  //   // setNum(num + 1);
+  //   console.log("レンダリング");
+  //   setNum((prev) => prev + 1);
+  // };
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんんちは!</h1>
       <ColoredMessage color="blue">お元気ですか</ColoredMessage>
       <ColoredMessage color="pink">元気です</ColoredMessage>
-      <button onClick={onClickButton}>ボタン</button>
-      <p>{num}</p>
-      <CssModules />
+      {/* <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p> */}
+      {/* <CssModules /> */}
+      <Emotion />
     </>
   );
 };
